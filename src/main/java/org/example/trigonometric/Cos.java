082a2c6c9cbf7b09bb48151e911c9bd9167d1b81
danil-emelin <danil228>
2025-03-20 10:54:48 -0400

    add trigonometric functions

---
+++ src/main/java/org/example/trigonometric/Cos.java
@@ -0,0 +1,49 @@
+package org.example.trigonometric;
+
+import ch.obermuhlner.math.big.BigDecimalMath;
+import org.example.function.LimitedIterationsExpandableFunction;
+
+import static java.math.BigDecimal.ONE;
+import static java.math.BigDecimal.ZERO;
+import static java.math.MathContext.DECIMAL128;
+import static java.math.RoundingMode.HALF_EVEN;
+
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+
+public class Cos extends LimitedIterationsExpandableFunction {
+
+    private final Sin sin;
+
+    public Cos() {
+        super();
+        this.sin = new Sin();
+    }
+
+    public Cos(final Sin sin) {
+        super();
+        this.sin = sin;
+    }
+
+    @Override
+    public BigDecimal calculate(final BigDecimal x, final BigDecimal precision)
+            throws ArithmeticException {
+        checkValidity(x, precision);
+
+        final MathContext mc = new MathContext(DECIMAL128.getPrecision(), HALF_EVEN);
+        final BigDecimal correctedX = x.remainder(BigDecimalMath.pi(mc).multiply(new BigDecimal(2)));
+
+        if (correctedX.compareTo(ZERO) == 0) {
+            return ONE;
+        }
+
+        final BigDecimal result =
+                sin.calculate(
+                        BigDecimalMath.pi(mc)
+                                .divide(new BigDecimal(2), DECIMAL128.getPrecision(), HALF_EVEN)
+                                .subtract(correctedX),
+                        precision);
+        return result.setScale(precision.scale(), HALF_EVEN);
+    }
+}
---
+++ src/main/java/org/example/trigonometric/Tan.java
@@ -0,0 +1,45 @@
+package org.example.trigonometric;
+
+
+import org.example.function.LimitedIterationsExpandableFunction;
+
+import static java.lang.String.format;
+import static java.math.BigDecimal.ZERO;
+import static java.math.MathContext.DECIMAL128;
+import static java.math.RoundingMode.HALF_EVEN;
+
+import java.math.BigDecimal;
+
+public class Tan extends LimitedIterationsExpandableFunction {
+
+    private final Sin sin;
+    private final Cos cos;
+
+    public Tan(final Sin sin, final Cos cos) {
+        super();
+        this.sin = sin;
+        this.cos = cos;
+    }
+
+    public Tan() {
+        super();
+        this.sin = new Sin();
+        this.cos = new Cos();
+    }
+
+    @Override
+    public BigDecimal calculate(final BigDecimal x, final BigDecimal precision)
+            throws ArithmeticException {
+        checkValidity(x, precision);
+
+        final BigDecimal sinValue = sin.calculate(x, precision);
+        final BigDecimal cosValue = cos.calculate(x, precision);
+
+        if (cosValue.compareTo(ZERO) == 0) {
+            throw new ArithmeticException(format("Function value for argument %s doesn't exist", x));
+        }
+
+        final BigDecimal result = sinValue.divide(cosValue, DECIMAL128.getPrecision(), HALF_EVEN);
+        return result.setScale(precision.scale(), HALF_EVEN);
+    }
+}
