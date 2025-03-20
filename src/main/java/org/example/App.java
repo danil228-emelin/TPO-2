de6e611152c59758a1ecd18051d5c9fe8cef7ebf
danil-emelin <danil228>
2025-03-20 12:25:37 -0400

    add main

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,7 +1,88 @@
 package org.example;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+import org.example.function.FunctionsSystem;
+import org.example.logarithmic.Ln;
+import org.example.logarithmic.Log;
+import org.example.trigonometric.Cos;
+import org.example.trigonometric.Sin;
+import org.example.trigonometric.Tan;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+public class App {
+
+    public static void main(String[] args) throws IOException {
+        final Cos cos = new Cos();
+        CsvWriter.write(
+                "./csv/cos.csv",
+                cos,
+                new BigDecimal(-1),
+                new BigDecimal(1),
+                new BigDecimal("0.1"),
+                new BigDecimal("0.0000000001"));
+
+        final Sin sin = new Sin();
+        CsvWriter.write(
+                "csv/sin.csv",
+                sin,
+                new BigDecimal(-1),
+                new BigDecimal(1),
+                new BigDecimal("0.1"),
+                new BigDecimal("0.0000000001"));
+
+        final Tan tan = new Tan();
+        CsvWriter.write(
+                "csv/tan.csv",
+                tan,
+                new BigDecimal(-1),
+                new BigDecimal(1),
+                new BigDecimal("0.1"),
+                new BigDecimal("0.0000000001"));
+
+        final Ln ln = new Ln();
+        CsvWriter.write(
+                "csv/ln.csv",
+                ln,
+                new BigDecimal(1),
+                new BigDecimal(20),
+                new BigDecimal("0.1"),
+                new BigDecimal("0.0000000001"));
+
+        final Log log3 = new Log(3);
+        CsvWriter.write(
+                "csv/log3.csv",
+                log3,
+                new BigDecimal(1),
+                new BigDecimal(20),
+                new BigDecimal("0.1"),
+                new BigDecimal("0.00000000001"));
+
+        final Log log5 = new Log(5);
+        CsvWriter.write(
+                "csv/log5.csv",
+                log5,
+                new BigDecimal(1),
+                new BigDecimal(20),
+                new BigDecimal("0.1"),
+                new BigDecimal("0.00000000001"));
+
+        final Log log10 = new Log(10);
+        CsvWriter.write(
+                "csv/log10.csv",
+                log10,
+                new BigDecimal(1),
+                new BigDecimal(20),
+                new BigDecimal("0.1"),
+                new BigDecimal("0.00000000001"));
+
+        final FunctionsSystem func = new FunctionsSystem();
+        CsvWriter.write(
+                "csv/func.csv",
+                func,
+                new BigDecimal(-2),
+                new BigDecimal(2),
+                new BigDecimal("0.1"),
+                new BigDecimal("0.00000000001"));
     }
 }
